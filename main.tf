931be7bc27bf2f7704d8d35755c4645dfaedab19
Shekar Reddy <34877715+shreddy0362@users.noreply.github.com>
2019-02-05 14:26:38 -0500

    Updated

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 resource "aws_iam_user" "this" {
-   //count = "${var.create_user && var.create_iam_user_login_profile ? 1 : 0}"
 
    count = 3
     name          = "${lookup(var.name, count.index)}"
@@ -7,11 +6,9 @@ resource "aws_iam_user" "this" {
     force_destroy = "${var.force_destroy}"
   }
 resource "aws_iam_user_login_profile" "this" {
-  //count = "${var.create_user && var.create_iam_user_login_profile ? 1 : 0}"
   count = 3
 
   user                    = "${lookup(var.name, count.index)}"
-  pgp_key                 = "${var.pgp_key}"
   password_length         = "${var.password_length}"
   password_reset_required = "${var.password_reset_required}"
 }
@@ -25,7 +22,6 @@ resource "aws_iam_group" "Analytics_Developer" {
     path = "/"
 }
 resource "aws_iam_group_membership" "admin-testuser" {
-    //count = "${var.create_user && var.upload_iam_user_ssh_key ? 1 : 0}"
     count = 2 
     name = "admin-user-membership"
     users = "${var.admin}"
